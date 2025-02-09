6831ee4b215b3a7351a149a9fffdc7c0094aa3fd
cubexy <54584804+cubexy@users.noreply.github.com>
2025-02-09 20:03:37 +0100

    todo

--- frontend/app/recipes/page.tsx
+++ frontend/app/recipes/page.tsx
@@ -1,23 +1,40 @@
-import { Avatar, Box, Card, Flex, Inset, Strong, Text } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Card,
+  Flex,
+  Heading,
+  Inset,
+  Strong,
+  Text
+} from "@radix-ui/themes";
 import Image from "next/image";
 
 const ListRecipes = () => {
   return (
-    <Card size="2" className="w-[500px] h-[266px]">
-      <Flex gap="2" align="center">
-        <Inset clip="padding-box" side="top" pb="current">
-          <div className="max-w-[200px] max-h-[200px]">
-            <Image
-              src="/recipe_placeholder.jpg"
-              alt="Rezeptbild"
-              fill
-              quality={75}
-              className="object-cover"
-            />
-          </div>
-        </Inset>
-      </Flex>
-    </Card>
+    <Flex gap="4" direction="column">
+      <Heading size="5">Rezepte</Heading>
+      <Card size="2" className="w-[550px] h-[300px] shadow-lg">
+        <Flex gap="2" align="center">
+          <Inset
+            clip="padding-box"
+            side="left"
+            pb="current"
+            className="shadow-2xl"
+          >
+            <div className="max-w-[200px] min-w-[200px] max-h-[300px] min-h-[300px] relative">
+              <Image
+                src="/recipe_placeholder.jpg"
+                alt="Rezeptbild"
+                fill
+                quality={75}
+                className="object-cover"
+              />
+            </div>
+          </Inset>
+        </Flex>
+      </Card>
+    </Flex>
   );
 };
 
