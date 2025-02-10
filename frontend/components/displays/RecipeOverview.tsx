86f00d9759f14ccdb66cbdd286d8ee27bfff9286
cubexy <54584804+cubexy@users.noreply.github.com>
2025-02-10 22:44:08 +0100

    refactor RecipeOverview component; adjust padding in Flex container

--- frontend/components/displays/RecipeOverview.tsx
+++ frontend/components/displays/RecipeOverview.tsx
@@ -20,9 +20,9 @@ export const RecipeOverview = ({ item }: { item: RecipeListItem }) => {
       <Link href={`/recipes/${item.id}`}>
         <Card
           className="absolute bottom-4 left-4 right-4 z-10 hover:bottom-5 transition-all"
-          variant="classic"
+          variant="classic" //ghost?
         >
-          <Flex className="p-2 max-w-[300px]" direction="column" gap="2">
+          <Flex className="p-1 max-w-[300px]" direction="column" gap="2">
             <Heading size="6" weight="medium">
               {item.title}
             </Heading>
