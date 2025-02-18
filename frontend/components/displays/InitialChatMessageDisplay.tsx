9e9f9a445841cf0342016a3f137c39aec735cf32
cubexy <54584804+cubexy@users.noreply.github.com>
2025-02-18 23:28:08 +0100

    update InitialChatMessageDisplay component; adjust Avatar size and add shadow effect

--- frontend/components/displays/InitialChatMessageDisplay.tsx
+++ frontend/components/displays/InitialChatMessageDisplay.tsx
@@ -10,7 +10,7 @@ export const InitialChatMessageDisplay = () => {
       gap="3"
       className="w-full"
     >
-      <Avatar fallback={<MagicWandIcon />} />
+      <Avatar fallback={<MagicWandIcon />} size={"2"} className="shadow-md" />
       <Text>
         Lass uns gemeinsam dein Rezept hinzufügen! Gib mir dazu etwa deine{" "}
         <Text weight="medium">verwendeten Zutaten</Text>, eine Liste von{" "}
